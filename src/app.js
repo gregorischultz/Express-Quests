9016cdd13e34c0567783f72313628c2db7edba73
Gregori Schultz <gregorischultz07@gmail.com>
2024-03-11 18:51:55 +0100

    quête express 03

--- src/app.js
+++ src/app.js
@@ -16,4 +16,7 @@ app.get ("/api/users/:id", userControllers.getUsersById);
 app.post ("/api/movies", movieControllers.postMovie);
 app.post ("/api/users", userControllers.postUser);
 
+app.put ("/api/movies/:id", movieControllers.updateMovie);
+app.put ("/api/users/:id", userControllers.updateUser);
+
 module.exports = app;
